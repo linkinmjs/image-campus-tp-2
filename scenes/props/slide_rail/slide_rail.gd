c808a6dbb48404b545c33073caf3dbfae9b1b5b9
unknown <linkinmjs@gmail.com>
2025-11-28 19:46:43 -0300

    Fix slide rail

--- scenes/props/slide_rail/slide_rail.gd
+++ scenes/props/slide_rail/slide_rail.gd
@@ -6,22 +6,40 @@ extends Node3D
 @onready var rail: MeshInstance3D = $Rail
 @onready var area_3d: Area3D = $Rail/Area3D
 
+@onready var path_3d: Path3D = $Path3D
+@onready var path_follow_3d: PathFollow3D = $Path3D/PathFollow3D
+@onready var marker_3d: Marker3D = $Path3D/PathFollow3D/Marker3D
+
+
+var slide_dir := 1.0
+var player_on_barrier: bool
+var player: CharacterBody3D
 
 func _ready() -> void:
+	player_on_barrier = false
 	area_3d.body_entered.connect(_on_body_entered)
 	area_3d.body_exited.connect(_on_body_exited)
-
-func _process(delta: float) -> void:
 	
+func _process(delta: float) -> void:
 	# Posiciones de los dos extremos
 	var a: Vector3 = start.global_position
 	var b: Vector3 = end.global_position
-
+	
 	var dir: Vector3 = b - a
 	var length := dir.length()
-	
 	var mid := a + dir * 0.5
 	
+	var curve := path_3d.curve
+	
+	# Pasamos las posiciones globales de los Marker3D
+	# a coordenadas locales del Path3D
+	var p0: Vector3 = path_3d.to_local(a)
+	var p1: Vector3 = path_3d.to_local(b)
+	
+	curve.clear_points()
+	curve.add_point(p0)
+	curve.add_point(p1)
+
 	# Orientar cilindro:
 	var up := dir.normalized() # eje Y del tubo
 	#print("up = dir.normalized()    >>    %s" % up)
@@ -40,11 +58,46 @@ func _process(delta: float) -> void:
 	# altura_final = 2 * scale.y  =>  scale.y = length / 2
 	rail.scale = Vector3(1.0, length * 0.5, 1.0)
 	
-func _on_body_entered(body: Node3D) -> void:
+	# A partir de acá manejamos la lógica del powerslide
+	if player_on_barrier and path_follow_3d.progress_ratio < 1:
+		path_follow_3d.progress_ratio += slide_dir * 0.2 * delta
+		player.global_position = marker_3d.global_position
+	
+	
+	
+func _on_body_entered(body: CharacterBody3D) -> void:
 	print("enter")
-	print(body.name)
+	if player_on_barrier:
+		return
+	player_on_barrier = true
+	GameManager.player_on_powerslide = true
+	player = body
+	
+	var closest_offset = path_3d.curve.get_closest_offset(path_3d.to_local(body.global_position))
+	var backed_length = path_3d.curve.get_baked_length()
+	var new_progress_ration = closest_offset / backed_length
+	
+	path_follow_3d.progress_ratio = new_progress_ration
+	
+	# Tangente del path donde cayó el player
+	# (PathFollow apunta "forward" en -Z de su basis)
+	var tangent := -path_follow_3d.global_transform.basis.z
+	
+	# Velocidad horizontal del player al entrar
+	var v := body.velocity
+	v.y = 0.0
+	if v.length() < 0.001:
+		# fallback si llegó casi quieto: usa vector desde player hacia la tangente
+		v = (path_follow_3d.global_transform.origin - body.global_transform.origin)
+		v.y = 0.0
+
+	# Decidir sentido: +1 avanza, -1 retrocede
+	slide_dir = 1.0 if v.normalized().dot(tangent.normalized()) >= 0.0 else -1.0
 
 
-func _on_body_exited(body: Node3D) -> void:
+
+func _on_body_exited(_body: CharacterBody3D) -> void:
 	print("exit")
-	print(body.name)
+	player_on_barrier = false
+	GameManager.player_on_powerslide = false
+	player = null
