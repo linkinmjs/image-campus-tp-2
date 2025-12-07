c77674d28b4ae71fc085296908fdff3d319b6301
unknown <linkinmjs@gmail.com>
2025-12-07 17:05:57 -0300

    Add zombie_01 ps1 style

---
+++ scenes/characters/zombie_01/zombie_attack.gd
@@ -0,0 +1,26 @@
+extends State
+class_name ZombieAttack
+
+@onready var enemy: CharacterBody3D = get_parent().get_parent()
+@onready var player: CharacterBody3D = enemy.player
+@onready var animation_tree: AnimationTree = $"../../AnimationTree"
+
+var animation_playback: AnimationNodeStateMachinePlayback
+var attacking: bool = false
+
+func _ready() -> void:
+	animation_playback = animation_tree.get("parameters/playback")
+
+func enter():
+	attacking = true
+	animation_playback.travel("attacking")
+
+func process(_delta: float):
+	if enemy.global_position.distance_to(player.global_position) > enemy.AttackReach and not attacking: 
+		emit_signal("Transitioned", self, "ZombieChase")
+
+func _attack_player():
+	var enemy_attack = Attack.new(15.0, enemy)
+	if enemy.global_position.distance_to(player.global_position) <= enemy.AttackReach:
+		player.health_component.damage(enemy_attack)
+	attacking = false
